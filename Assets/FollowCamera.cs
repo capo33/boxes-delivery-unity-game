2daecb3779cd89bb90d5283a77e854c1debc4d29
Mohamed <s2000588@edu.bc.fi>
2023-12-14 22:33:32 +0200

    add follow camera script

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] GameObject thingsToFollow;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = thingsToFollow.transform.position + new Vector3(0, 0, -10);
+    }
+}

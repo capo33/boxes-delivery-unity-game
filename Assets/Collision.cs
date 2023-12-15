4d7aaa3505642b1ba5a6c5de9f678eb528be8c0e
Mohamed <s2000588@edu.bc.fi>
2023-12-15 21:08:39 +0200

    work with Destroy method

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+    bool hasPackage;
+    float destroyDelay = 0.3f;
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Ouch!");
     }    
 
     void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("I got a trigger");
+        if(other.CompareTag("Package") && !hasPackage) // !hasPackage for Destroy & !hasPackage = false
+        {
+            Debug.Log("You picked up a package!");  
+            // We put hasPackage = true to prove that we have a package
+            hasPackage = true;
+            Destroy(other.gameObject,destroyDelay);
+        }
+        if(other.CompareTag("Customer") && hasPackage)
+        {
+            Debug.Log("You delivered a package!");
+            // We put hasPackage back to false to prevent deliver the package over and over again
+            hasPackage = false;
+         }
     }
 }
